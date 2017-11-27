01c90ac5def518d761aff346f57854e3a2314a75
Stojan Kukrika <stojan.kukrika@gmail.com>
2017-11-27 15:53:20 +0100

    begin with optimise database

---
+++ src/stojankukrika/PaxumPayment/database/migrations/2017_11_24_100200_transactions.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Transactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transactions');
+    }
+}
